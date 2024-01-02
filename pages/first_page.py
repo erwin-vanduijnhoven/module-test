89d1b74e7cd2a93679585542f311d034b1c2d6ce
erwin-vanduijnhoven <131747532+erwin-vanduijnhoven@users.noreply.github.com>
2024-01-02 17:11:37 +0100

    Update first_page.py

--- pages/first_page.py
+++ pages/first_page.py
@@ -13,41 +13,13 @@ auth = ClarifaiAuthHelper.from_streamlit(st)
 stub = create_stub(auth)
 userDataObject = auth.get_user_app_id_proto()
 
-st.title("Simple example to list inputs")
+# Stream inputs from the app. list_inputs give list of dictionaries with inputs and its metadata .
+input_obj = User(user_id=userDataObject.user_id).app(app_id=userDataObject.app_id).inputs()
+all_inputs = input_obj.list_inputs(per_page=2)
 
-with st.form(key="data-inputs"):
-  mtotal = st.number_input(
-      "Select number of inputs to view in a table:", min_value=5, max_value=100)
-  submitted = st.form_submit_button('Submit')
+for inp in range(len(all_inputs)):
+  input = all_inputs[inp]
+  st.image(input.data.image.url, caption='Image')
+  for region in input.data.regions:
+    st.image(region.data.image.url, caption='region')
 
-if submitted:
-  if mtotal is None or mtotal == 0:
-    st.warning("Number of inputs must be provided.")
-    st.stop()
-  else:
-    st.write("Number of inputs in table will be: {}".format(mtotal))
-
-  # Stream inputs from the app. list_inputs give list of dictionaries with inputs and its metadata .
-  input_obj = User(user_id=userDataObject.user_id).app(app_id=userDataObject.app_id).inputs()
-  all_inputs = input_obj.list_inputs()
-
-  #Check for no of inputs in the app and compare it with no of inputs to be displayed.
-  if len(all_inputs) < (mtotal):
-    raise Exception(
-        f"No of inputs is less than {mtotal}. Please add more inputs or reduce the inputs to be displayed !"
-    )
-
-  else:
-    data = []
-    #added "data_url" which gives the url of the input.
-    for inp in range(mtotal):
-      data.append({
-          "id": all_inputs[inp].id,
-          "data_url": all_inputs[inp].data.image.url,
-          "status": all_inputs[inp].status.description,
-          "created_at": timestamp_pb2.Timestamp.ToDatetime(all_inputs[inp].created_at),
-          "modified_at": timestamp_pb2.Timestamp.ToDatetime(all_inputs[inp].modified_at),
-          "metadata": json_format.MessageToDict(all_inputs[inp].data.metadata),
-      })
-
-  st.dataframe(data)
